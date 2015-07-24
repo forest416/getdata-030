85eda6f82ebb4777c75b60f24759c88480bb67ce
forest416 <forest416@gmail.com>
2015-07-23 22:50:06 -0700

    update readme.md and run_analysis.R

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # getdata-030 
+
+## prepare
+Copy the R program file run_analysis.R to the same directory as file *features* file 
+
+##File read in process
+
+- features
+- activity_labels
+- train/subject_train
+- train/X_train
+- train/y_train
+- test/subject_train
+- test/X_train
+- test/y_train
+
+## steps of processing
+* read all file 
+* assign variable name
+* merge test observes with activity label and subject number
+* merge train observes with activity label and subject number
+* merge test and train data
+
+* calculate mean by activity and subject and save to file
--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,32 @@
 
 setwd("C:/Users/jin/Desktop/clean.data/FUCI/UCI HAR Dataset")
 
+## read all input files
+
 test_y=read.csv('test/y_test.txt',header=F)
 train_y=read.csv('train/y_train.txt',header=F)
 
 names(test_y) = "ActivityNo"
 names(train_y) = "ActivityNo"
 
+## read subject file, each row in this file link to X_ file
 test_sub=read.csv('test/subject_test.txt',header=FALSE)
 names(test_sub) = 'subject'
 train_sub=read.csv('train/subject_train.txt',header=FALSE)
 names(train_sub) = 'subject'
 
 
-##
+## read activity file
 activity=read.delim('activity_labels.txt', F," ")
 names(activity) = c("ActivityNo", "ActivityName")
 
 feature=read.delim('features.txt',sep=' ',header=F)
+# featureCount is referenced when read X_ file
 featureCount=nrow(feature)
 
 names(feature )= c("featureNo", "featureName")
 
+## read observation file
 test_x=read.fwf('test/X_test.txt',rep(16,featureCount))
 names(test_x) = feature$featureName
 
@@ -29,24 +34,24 @@ train_x=read.fwf('train/X_train.txt',rep(16,featureCount))
 names(train_x) = feature$featureName
 
 
-
+## replace  activityNo to ActivityName
 train_activity=merge(activity, train_y, by.x="ActivityNo", by.y="ActivityNo")$ActivityName
 
-train=cbind("TYPE"="TRAIN",Activity=train_activity, train_sub, train_x)
+## train data in one
+train=cbind(Activity=train_activity, train_sub, train_x)
 
 
-
+## replace activityNo to ActivityName
 test_activity=merge(activity, test_y, by.x="ActivityNo", by.y="ActivityNo")$ActivityName
+# test data in one
+test=cbind(Activity=test_activity, test_sub, test_x)
 
-test=cbind("TYPE"="TEST", Activity=test_activity, test_sub, test_x)
-
-
+## merge test and train data
 result=rbind(test,train)
-
+# a copy of all
 write.table(result, file="result.ALL.txt", row.name=FALSE, quote=TRUE)
 
-result$TYPE <- NULL
-
+# mean by activity and subject
 step5=(aggregate(. ~   Activity +subject, data=result,FUN=mean))
 write.table(step5, file="step5.txt", row.name=FALSE, quote=TRUE)
 
